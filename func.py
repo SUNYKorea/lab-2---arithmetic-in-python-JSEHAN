23b67a5bf243d3c6f50703e20f9938cb152dfbe1
JSEHAN <jshwow8304@gmail.com>
2023-03-20 17:34:54 +0900

    Finalized lab

--- func.py
+++ func.py
@@ -5,12 +5,25 @@ def add(x, y):
 # TODO: Add definitions of sub(), div(), mult(), exp(), as well as neg() and sqrt().
 #       neg() should return the negation of the given number, and sqrt() should
 #       return the square root of the given number. 
+    
+
+def sub(x, y):
+    return x - y
+
+def div(x, y):
+    return x / y
+
+def mult(x, y):
+    return x * y
 
 def neg(x):
-    ... # fill here
+    return -x
+
+def exp(x,y):
+    return x**y
 
 def sqrt(x):
-    ... # fill here
+    return x**0.5
 
 # -------------------------------------- Task 2 -----------------------------------
 
@@ -18,12 +31,19 @@ def sqrt(x):
 #       Do not use arithmetic operators directly. 
 #       You're allowed to define other functions.
 a = 1
-b = -3
+b = -4
 c = 1
 
-x1 = ... # TODO: write a code to compute the first root of the quadratic equation
-x2 = ... # TODO: then do the same for the second root
-# Note: Make sure to remove the ellipsis (...) when you're writing your code
+# quadratic equation ax**2 + bx + c = 0, where a, b, c are real numbers and a =/= 0
+# (-b +/- (b**2 - 4 * a * c) ** 0.5) / (2 * a)
+import cmath
+d = c^2 - 4 * a * c
+print: cmath
+denom = mult(2,a)
+nom1 = add(neg(b), sqrt(sub(exp(b,2), (mult(mult(4, a),c)))))
+nom2 = sub(neg(b), sqrt(sub(exp(b,2), (mult(mult(4, a),c)))))
 
+x1 = nom1 / denom 
+x2 = nom2 / denom
 print("First root:" + str(x1))
 print("Second root:" + str(x2))
